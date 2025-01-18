3483907fb8fd6e9a2bfc28bc0be632bec69dfe19
Jeet Daiya <jeet.a.daiya24@gmail.com>
2025-01-18 06:56:53 +0530

    Created Abstraction between firebase service and the frontend.

---
+++ firebase_app/lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,35 @@
+class UserNotFoundAuthException implements Exception{
+
+}
+
+class WrongPasswordAuthException implements Exception{
+
+}
+
+// Register Exceptions
+
+class WeakPasswordAuthException implements Exception{
+
+}
+
+class EmailAlreadyInUseAuthException implements Exception{
+
+}
+
+class InvalidAuthException implements Exception{
+
+}
+
+// Generic Exceptions
+
+
+
+
+
+class GenericAuthException implements Exception{
+
+}
+
+class UserNotLoggedInAuthException implements Exception{
+
+}
